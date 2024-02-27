2022d0ba54de4387722115e275a01263b80ac39c
calebmutisya <mutisyacalebmusau@gmail.com>
2024-02-27 10:55:05 +0300

    delete task edited

--- server/app.py
+++ server/app.py
@@ -253,7 +253,7 @@ def update_task(id):
 @app.route('/delete_task/<int:id>', methods=['DELETE'])
 @jwt_required()
 def delete_task(id):
-    task = Task.query.get_or_404(id)
+    task = FormData.query.get_or_404(id)
     db.session.delete(task)
     db.session.commit()
     return jsonify({'message': 'Task deleted successfully'})
